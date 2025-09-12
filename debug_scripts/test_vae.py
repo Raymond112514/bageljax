ea3ddc6d5ab0f778c2da78f3ddc655e7e442cb4a
pranavatreya <pranavatreya@berkeley.edu>
2025-09-11 23:14:27 -0700

    tested that vae can reconstruct stacked droid images

--- debug_scripts/test_vae.py
+++ debug_scripts/test_vae.py
@@ -11,9 +11,10 @@ from bageljax.autoencoder import build_autoencoder
 flax.config.update('flax_use_orbax_checkpointing', False)
 
 # Load the source image
-src_img = Image.open("colors.png")
+src_img = Image.open("/home/pranav/bageljax/samples/stacked_droid.png")
 src_img_np = np.array(src_img)
-assert src_img_np.shape == (1024, 1024, 3)
+src_img_np = src_img_np[:, :, :-1]
+assert src_img_np.shape == (672, 560, 3), f"Got shape {src_img_np.shape}"
 assert src_img_np.dtype == np.uint8
 src_img_np = 2 * (src_img_np.astype(np.float32) / 255.0) - 1
 src_img_np = src_img_np[None, ...] # add a batch dimension
@@ -46,7 +47,7 @@ ae_variables = ae.init(key, jnp.zeros((1, 1024, 1024, 3)))
 print("Autoencoder initialized.")
 
 # Load weights for the autoencoder
-ae_checkpoint_path = "/home/pranav/bageljax/new_ae_ckpt"
+ae_checkpoint_path = "/home/pranav/bageljax/pretrained_weights/ae"
 ae_variables = checkpoints.restore_checkpoint(ae_checkpoint_path, target=ae_variables)
 print("Autoencoder weights loaded.")
 
@@ -56,11 +57,11 @@ ae_decode = make_jitted_decode(ae)
 # Encode and decode test
 rng, key = jax.random.split(rng)
 img_encoded = ae_encode(ae_variables, src_img_np, key)
-assert img_encoded.shape == (1, 128, 128, 16) and img_encoded.dtype == jnp.float32
+assert img_encoded.shape == (1, 84, 70, 16) and img_encoded.dtype == jnp.float32
 
 img_decoded = ae_decode(ae_variables, img_encoded)
 img_decoded = np.array(img_decoded)[0]
 img_decoded = np.clip((img_decoded + 1) * 127.5, 0, 255).astype(np.uint8)
 recon = Image.fromarray(img_decoded)
 
-recon.save("recon.png")
+recon.save("/home/pranav/bageljax/samples/recon.png")
